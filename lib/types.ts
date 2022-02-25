e90f65dd8da05421288e3369aa95ee896d1e3066
Marwan <saadmarwan16@gmail.com>
2022-02-25 19:35:24 +0300

    Solved typescipt eslint errors

--- lib/types.ts
+++ lib/types.ts
@@ -2,3 +2,14 @@ import { User } from "firebase/auth";
 
 export type user = User | null | undefined;
 export type username = string | null;
+export type post = {
+    content: string;
+    createdAt: number;
+    heartCount: number;
+    published: boolean;
+    slug: string;
+    title: string;
+    uid: string;
+    updatedAt: number;
+    username: string;
+}
